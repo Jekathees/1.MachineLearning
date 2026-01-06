3677839d0ee23915577686b656742a6238b79ed2
Jekathees <jekajekathees2002@gmail.com>
2026-01-06 20:06:40 +0530

    Add files via upload

---
+++ Diabetes/main.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import pickle
+
+diabetes_dataset = pd.read_csv('diabetes.csv')
+counts=diabetes_dataset['Outcome'].value_counts()
+print(counts)
+values=diabetes_dataset.groupby('Outcome').mean()
+print(values)
+
+X = diabetes_dataset.drop(columns='Outcome', axis=1)
+Y = diabetes_dataset['Outcome']
+
+scaler = StandardScaler()
+scaler.fit(X)
+standardized_data = scaler.transform(X)
+X = standardized_data
+Y = diabetes_dataset['Outcome']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+
+classifier = svm.SVC(kernel='linear')
+classifier.fit(X_train, Y_train)
+
+X_test_prediction = classifier.predict(X_test)
+accuracy=accuracy_score(X_test_prediction, Y_test)  
+print('Accuracy:', accuracy)     
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+input_data_as_numpy_array = np.asarray(input_data)
+input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+std_data = scaler.transform(input_data_reshaped)
+prediction = classifier.predict(std_data)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('The person is not diabetic')
+else:   
+  print('The person is diabetic')
+
+pickle.dump(classifier, open("Diabetes.pkl", "wb"))
+pickle.dump(scaler, open("scaler.pkl", 'wb'))
